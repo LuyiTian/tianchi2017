cb324ed739e65e6caefe37effeafd744eaa47d2e
Luyi Tian <tlytiger@hotmail.com>
2017-01-19 15:25:25 +0800

    update config

--- config.py
+++ config.py
@@ -7,9 +7,11 @@ USER_NAME = os.uname()[1]
 
 if USER_NAME == 'Dajuns-MacBook-Pro.local':
     BASE_PATH = '/Users/luodajun/Documents/workspace/tianchi2017'
+elif USER_NAME == 'LuyiTians-MacBook-Pro.local':
+    BASE_PATH = '/Users/luyi/data/tianchi2017/'
 elif USER_NAME == '':
     BASE_PATH = ''
 elif USER_NAME == '':
-    BASE_PATH = ''
+    BASE_PATH = ''  # aliyun linux server
 else:
     raise Exception('Invalid Machine Name %s' % USER_NAME)
