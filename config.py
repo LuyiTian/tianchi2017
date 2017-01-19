df7d6b8e0e76450dc57a5f3134a65a6128ac0868
bianbeilei <bianbeilei@wegene.com>
2017-01-19 21:11:03 +0800

    update

--- config.py
+++ config.py
@@ -9,8 +9,8 @@ if USER_NAME == 'Dajuns-MacBook-Pro.local':
     BASE_PATH = '/Users/luodajun/Documents/workspace/tianchi2017'
 elif USER_NAME == 'LuyiTians-MacBook-Pro.local':
     BASE_PATH = '/Users/luyi/data/tianchi2017/'
-elif USER_NAME == '':
-    BASE_PATH = ''
+elif USER_NAME == 'MacBook-pro':
+    BASE_PATH = '/Users/bianbeilei/tianchi2017'
 elif USER_NAME == '':
     BASE_PATH = ''  # aliyun linux server
 else:
