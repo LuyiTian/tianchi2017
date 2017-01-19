3ccf7a94c7b892d0f0dd6afa5e361b9ee9462f55
Dajun Luo <luodj@wegene.com>
2017-01-19 21:23:51 +0800

    add data path

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ USER_NAME = os.uname()[1]
 
 if USER_NAME == 'Dajuns-MacBook-Pro.local':
     BASE_PATH = '/Users/luodajun/Documents/workspace/tianchi2017'
+    DATA_PATH = os.path.join(BASE_PATH, 'dataset')
 elif USER_NAME == '':
     BASE_PATH = ''
 elif USER_NAME == '':
