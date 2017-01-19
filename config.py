b61694fa20d1eb1aa959d3eb7a84238bbdb3520b
Dajun Luo <luodj@wegene.com>
2017-01-19 15:13:49 +0800

    add path resolver

---
+++ config.py
@@ -0,0 +1,15 @@
+"""Configure path and dependency."""
+
+import os
+
+
+USER_NAME = os.uname()[1]
+
+if USER_NAME == 'Dajuns-MacBook-Pro.local':
+    BASE_PATH = '/Users/luodajun/Documents/workspace/tianchi2017'
+elif USER_NAME == '':
+    BASE_PATH = ''
+elif USER_NAME == '':
+    BASE_PATH = ''
+else:
+    raise Exception('Invalid Machine Name %s' % USER_NAME)
