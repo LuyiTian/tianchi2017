23a8da63357e05b9772c88c55016a1bd04a3c71d
Luyi Tian <luyit@wegene.com>
2017-01-28 11:34:07 +0800

    update aliyun config

--- config.py
+++ config.py
@@ -14,5 +14,8 @@ elif USER_NAME == 'LuyiTians-MacBook-Pro.local':
 elif USER_NAME == 'MacBook-pro':
     BASE_PATH = '/Users/bianbeilei/tianchi2017'
     DATA_PATH = os.path.join(BASE_PATH, 'dataset')
+elif USER_NAME == 'WeAncestry.com':
+    BASE_PATH = '/data/tianchi2017/'
+    DATA_PATH = os.path.join(BASE_PATH, 'dataset')
 else:
     raise Exception('Invalid Machine Name %s' % USER_NAME)
